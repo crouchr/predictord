78a8ae5c5fb4133560f1ca9ea10f5ef40c74fd78
crouchr <richard.crouch@vonage.com>
2021-01-08 15:24:30 +0000

    Commit before forking to mvp branch

--- app/connect_db.py
+++ app/connect_db.py
@@ -5,6 +5,8 @@ def connect_database(mysql_host, db_name):
 
     :return:
     """
+    print('Accessing MySQL host ' + mysql_host + ' ...')
+
     mydb = mysql.connector.connect(
         host=mysql_host,
         database=db_name,
