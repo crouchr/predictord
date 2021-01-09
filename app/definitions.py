eb3a5607234776d98d9b41eee2417600a11e296e
crouchr <richard.crouch@vonage.com>
2021-01-09 09:37:04 +0000

    Commit before forking to mvp branch

--- app/definitions.py
+++ app/definitions.py
@@ -1,6 +1,7 @@
 # forecaster-service microservice
-# when using IDE
-endpoint_base = 'http://172.27.0.2:9501'
 
-# when using container
-#endpoint_base = 'http://forecaster-service:9501'
+# Option 1 - when using IDE
+#endpoint_base = 'http://172.27.0.2:9501'
+
+# Option2 - when using container
+endpoint_base = 'http://forecaster-service:9501'
