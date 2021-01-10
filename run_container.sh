8cd5180adbd5823cdf848567a407af58d6562a73
crouchr <richard.crouch@vonage.com>
2021-01-10 11:06:11 +0000

    Commit before forking to mvp branch

--- run_container.sh
+++ run_container.sh
@@ -1,2 +1,2 @@
 #docker container run -it --device /dev/video0:/dev/video0 -v "$PWD"/sm:/sm ubuntu bash
-docker container run -it --device /dev/video0:/dev/video0 registry:5000/predictord:1.0.16
+docker container run -it --device /dev/video0:/dev/video0 registry:5000/predictord:1.0.17
